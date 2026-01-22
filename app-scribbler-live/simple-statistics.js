a7b814584bb3bb2c7008dfe6ebfca3ac1f74730b
jmtt646089 <jadeseala@gmail.com>
2026-01-23 11:34:42 +1300

    Update simple-statistics.js

--- app-scribbler-live/simple-statistics.js
+++ app-scribbler-live/simple-statistics.js
@@ -1,3 +1,7 @@
+// usable in all browsers
 await scrib.loadScript('https://unpkg.com/simple-statistics@7.8.8/dist/simple-statistics.js');
-
 sumSimple([1,2,3]);
+
+// usable in Google Chrome
+var stats = await import('https://cdn.skypack.dev/simple-statistics');
+console.log(stats.sumSimple([1, 2, 3]));
